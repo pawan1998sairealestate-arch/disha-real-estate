72e8dac5edfc3b6e20eeb1edb707436628755d3a
pawan1998sairealestate-arch <pawan.1998sairealestate@gmail.com>
2026-09-02 14:23:28 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='disha-crm-v6';
-const ASSETS=['./','./index.html','./manifest.json','./logo.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET') e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x}).catch(()=>caches.match('./'))))});
